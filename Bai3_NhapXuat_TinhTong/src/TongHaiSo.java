8da346bd6483ff1e79690b5540534177cdc94de7
kiettnacc25tth-eng <kiet.tna.cc25tth@ntu.edu.vn>
2026-08-18 22:13:03 +0700

    Thêm mã nhập khai báo biến

--- Bai3_NhapXuat_TinhTong/src/TongHaiSo.java
+++ Bai3_NhapXuat_TinhTong/src/TongHaiSo.java
@@ -1,8 +1,15 @@
 
+import java.util.Scanner;
 public class TongHaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Khai báo
+		double num1, num2, sum;
+		// Khai báo đổi tượng Scanner
+		Scanner banPhim = new Scanner(System.in);
+		// in ra thông tin, mới User nhập số thứu nhất
+		System.out.print("Mời nhập số thứ nhất: ");
+		num1= banPhim.nextDouble();
 
 	}
 
