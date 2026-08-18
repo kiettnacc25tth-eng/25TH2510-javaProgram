dac956109d8287b353015c60ab4f90b56f4cd444
kiettnacc25tth-eng <kiet.tna.cc25tth@ntu.edu.vn>
2026-08-18 21:12:40 +0700

    bài 1

---
+++ bai_1_hello/src/bai_1_hello/SayHelloApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bai_1_hello;
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin Chao. Toi Ten Tran Nguyen Anh Kiet");
+
+	}
+
+}
