8d081fd0cc77a7d830fdcf2ec489b80a76fb38be
kiettnacc25tth-eng <kiet.tna.cc25tth@ntu.edu.vn>
2026-08-18 21:38:49 +0700

    Thêm các lệnh khai báo, và tính toán

---
+++ Bai2_BienPhepToan/src/BaiTap2.java
@@ -0,0 +1,15 @@
+
+public class BaiTap2 {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	// 1. Khai báo các biến
+		int a = 15;
+		int b = 4;
+		double c = 3.5;
+	// 2. Tính toán
+		int long = a + b;
+		double thuong = a / c;
+	}
+
+}
