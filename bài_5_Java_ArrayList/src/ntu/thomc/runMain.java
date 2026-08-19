ea422f51c9cd99dcb5537202b10acfe9c1ef2ea5
kiettnacc25tth-eng <kiet.tna.cc25tth@ntu.edu.vn>
2026-08-19 21:45:02 +0700

    bài 5 nhập code

--- bài_5_Java_ArrayList/src/ntu/thomc/runMain.java
+++ bài_5_Java_ArrayList/src/ntu/thomc/runMain.java
@@ -1,10 +1,22 @@
 package ntu.thomc;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// khai báo biến danh sách,  chứa các số nguyên
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		// a.Nhập 10 phần tử từ bàn phím
+		Scanner banPhim = new Scanner(System.in);
+	for (int i=0; i<10; i++) {
+		// Nhập 1 phần tử
+		// In ra một hướng dẫn cho người dùng
+		System.out.print("Mời nhập phần tử thứ 1: ");
+		// cho User nhập, cất vào biến tạm
+		Integer tam = banPhim.nextInt();
+		// Thêm vào cuối danh sách
+		dsSoNguyen.add(tam);
+	}	
 	}
 
 }
